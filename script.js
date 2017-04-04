b87a076cb1a54a4ca0de47a24d0f20f61fb45e80
Hector Sanchez <hsanchez7934@gmail.com>
2017-04-04 16:59:13 -0600

    moved line of text in js. file

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
     console.log("clicked");
     var addTitle = $('#titleInput').val();
     var addBody = $('#bodyInput').val();
+    $('h2').append('<div class="newtitle">' + addTitle + '</div>');
+    $('#bodyTextContainer').append('<div class="newBodyText">' + addBody +
+    '</div>');
+  })
 
     // NO GOD FUNCTIONS - Write individual/reusable functions where you can
     // For prepend - Instead of targeting individual elements, try targeting the section containing the guts of the card
@@ -10,14 +14,10 @@
     // For the upvote arrays (look at for loops) or objects
     // CONSOLE LOG EVERYTHING!!!!
     // ES6 back ticks
-    $('h2').append('<div class="newtitle">' + addTitle + '</div>');
-    $('#bodyTextContainer').append('<div class="newBodyText">' + addBody +
-      '</div>');
       // function clearInputs () {
       //   $(this).val('');
       //   $(this).val('');
       // });
-  })
 
 // $(".save-button").on('click', function(){
 //     alert('test');
