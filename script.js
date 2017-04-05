11bae13299246e7e987fe7358bc571c5dd65199f
Sasha Dunbar <dunbarsa@protonmail.com>
2017-04-05 12:43:58 -0600

    scripts

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
-//
-//   $('.save-button').on('click',function() {
-//     console.log("clicked");
-//     var addTitle = $('#titleInput').val();
-//     var addBody = $('#bodyInput').val();
-//     $('.idea-container').prepend("<div class='one-idea'><h2 id='title-text'>" + addTitle +
-//     "</h2><button type='button' id='delete-button' alt=''><img src='icons/delete.svg'></button> \
-//     <p id='body-text'>" + addBody + "</p> \
-//     <article><img class='arrows' src='icons/upvote.svg' alt='Upvote icon.'> \
-//     <img class='arrows' src='icons/downvote.svg' alt='Downvote icon.'> \
-//     <div class='quality-box'><p>quality:</p><p class='var-quality'></p> \
-//     </div></article></div>");
-//     // $('h2').append('<div class="newtitle">' + addTitle + '</div>');
-//     // $('#bodyTextContainer').append('<div class="newBodyText">' + addBody +
-//     // '</div>');
-//     $('#delete-button').on('click', function() {
-//       console.log("I work");
-//       $(this).parent().remove();
-//     })
-//     clearCard();
-//   });
-// // <img src='icons/delete.svg'>
-// function clearCard () {
-//   $('#titleInput').val('');
-//   $('#bodyInput').val('');
-// }
 
 
 
-    // NO GOD FUNCTIONS - Write individual/reusable functions where you can
-    // For prepend - Instead of targeting individual elements, try targeting the section containing the guts of the card
-    // Consider prepending -- it'll keep things in chronological order
-    // For the upvote arrays (look at for loops) or objects
-    // CONSOLE LOG EVERYTHING!!!!
-    // ES6 back ticks
-      // function clearInputs () {
-      //   $(this).val('');
-      //   $(this).val('');
-      // });
 
-// $(".save-button").on('click', function(){
-//     alert('test');
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -79,8 +88,12 @@ $('.save-button').on('click',function() {
   $('#upvote').on('click', function(){
     if ($('.var-quality').text('swill')) {
       $('.var-quality').text('plausible');
-
-  }});
+    } else if ($('.var-quality').text('plausible')) {
+      $('.var-quality').text('genius');
+    } else if ($('.var-quality').text('genius')) {
+      $('.var-quality').text('genius');
+    }
+  });
 
 });
 
