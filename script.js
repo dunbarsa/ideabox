e95fb7fd6f48a5c2f1b2c56c6ff460a86acf87f2
Sasha Dunbar <dunbarsa@protonmail.com>
2017-04-05 15:08:53 -0600

    quality arrows and back ticks

--- script.js
+++ script.js
@@ -1,76 +1,27 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $('.save-button').on('click',function() {
-    console.log("clicked");
+    console.log("Save button has been clicked");
     var addTitle = $('#titleInput').val();
     var addBody = $('#bodyInput').val();
-    $('.idea-container').prepend("<div class='one-idea'><h2 id='title-text'>" + addTitle +
-    "</h2><button type='button' id='delete-button'><img src='icons/delete.svg' alt='Delete button'></button> \
-    <p id='body-text'>" + addBody + "</p> \
-    <article><button class='arrows' id='upvote'><img src='icons/upvote.svg' alt='Upvote icon.'></button> \
-    <button class='arrows' id='downvote'><img src='icons/downvote.svg' alt='Downvote icon.'></button> \
-    <div class='quality-box'><p>quality: </p>&ensp;<p class='var-quality'>swill</p> \
-    </div></article></div>");
-    // $('h2').append('<div class="newtitle">' + addTitle + '</div>');
-    // $('#bodyTextContainer').append('<div class="newBodyText">' + addBody +
-    // '</div>');
+    $('.idea-container').prepend(`
+      <div class='one-idea'>
+        <h2 id='title-text'>${addTitle}</h2>
+        <button type='button' id='delete-button'>
+          <img src='icons/delete.svg' alt='Delete button'>
+        </button>
+        <p id='body-text'>${addBody}</p>
+        <article>
+          <button class='arrows' id='upvote'>
+            <img src='icons/upvote.svg' alt='Upvote icon.'>
+          </button>
+          <button class='arrows' id='downvote'>
+            <img src='icons/downvote.svg' alt='Downvote icon.'>
+          </button>
+          <div class='quality-box'>
+            <p>quality: </p>&ensp;<p class='var-quality'>swill</p>
+          </div>
+        </article>
+      </div>`);
+
     $('#delete-button').on('click', function() {
       console.log("I work");
       $(this).parent().remove();
@@ -86,15 +37,15 @@ $('.save-button').on('click',function() {
 
   //correlate UPvote arrows with displayed quality state.
   $('#upvote').on('click', function(){
-    if ($('.var-quality').text('swill')) {
-      $('.var-quality').text('plausible');
-    } else if ($('.var-quality').text('plausible')) {
-      $('.var-quality').text('genius');
-    } else if ($('.var-quality').text('genius')) {
-      $('.var-quality').text('genius');
+    var $ideaQuality = $(this).parent().find('.var-quality');
+    if ($ideaQuality.text() == 'swill') {
+      $ideaQuality.text('plausible');
+    } else if ($ideaQuality.text() == 'plausible') {
+      $ideaQuality.text('genius');
+    } else if ($ideaQuality.text() == 'genius') {
+      $ideaQuality.text('genius');
     }
   });
-
 });
 
   //delete single card when trash is clicked
